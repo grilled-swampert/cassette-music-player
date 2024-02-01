181c36733330c73fc7cdd6f75eea66699aa150af
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2024-02-01 22:32:47 +0530

    scrolling feature

---
+++ public/src/index.js
@@ -0,0 +1,30 @@
+const stickySections = [...document.querySelectorAll('.sticky')];
+let images = [
+    '/images/groceries.png',
+    '/images/relaxing.png',
+    '/images/robot.png',
+    '/images/swinging.png'
+];
+
+images.forEach(img => {
+    stickySections.forEach(section => {
+        let image = document.createElement('img');
+        image.src = img;
+        section.querySelector('.scroll_section').appendChild(image);
+    })
+}) 
+
+window.addEventListener('scroll', (e) => {
+    for(let i = 0; i < stickySections.length; i++) {
+        transform(stickySections[i]);
+    }
+})
+
+function transform(section) {
+    const offsetTop = section.parentElement.offsetTop;
+    const scrollSection = section.querySelector('.scroll_section');
+     
+    let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+    percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage; 
+    scrollSection.style.transform = `translate3D(${-(percentage)}vw, 0, 0)`;
+}
